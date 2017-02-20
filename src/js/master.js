f7ed23afac336d24a739cf7d21f65a576718c8a0
GeordiD_eddypc <geordi.dosher@gmail.com>
2017-02-20 00:04:52 -0600

    Figured out how to calculate margin for chords. still working on how to get the chord that information in a clean way

--- src/js/master.js
+++ src/js/master.js
@@ -2,55 +2,14 @@
  * Created by GeordiD on 2/15/17.
  */
 
-/*
-$(document).ready(function() {
-    $("#edit").each(function(k,v) {
-        //console.log(k + " " + v);
-    });
-});
-*/
-
-function getTextFile(file)
-{
-    var allText;
-
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                allText = rawFile.responseText;
-            }
-        }
-    }
-    rawFile.send(null);
-
-    return allText;
-}
-
-function getPreviewHtml() {
-    var str = getTextFile("src/testFiles/test.txt");
-
-    var output = "";
-
-    for(var i = 0, len = str.length; i < len; i++) {
-        output+=str.charAt(i);
-        output+=".";
-    }
-
-    return output;
+function getPadding(element) {
+    return  (element.innerWidth() - element.width())/2;
 }
 
 $(document).ready(function() {
 
-    console.log("here");
+    var atest = $('#splendor').position().left - getPadding($('.chart-content'));
+    var test = atest + ($("#splendor").width()) / 2;
 
-
-    $('#edit').html(getTextFile("src/testFiles/test.txt"));
-
-    $('#preview').html(getPreviewHtml());
-
+    console.log(test);
 });
