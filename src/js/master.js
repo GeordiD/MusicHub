a61f711304e42e4ea95530f6db3de365147e1f8f
George  Dosher III <cgartist15@gmail.com>
2017-02-23 20:44:54 -0600

    uploading to win

--- src/js/master.js
+++ src/js/master.js
@@ -2,8 +2,18 @@
  * Created by GeordiD on 2/15/17.
  */
 
+var widthLine = 400; //px
+
 function getPadding(element) {
     return  (element.innerWidth() - element.width())/2;
+}
+
+function testSetLineBreaks() {
+
+    
+
+    var test = text;
+    console.log(test);
 }
 
 $(document).ready(function() {
@@ -12,5 +22,11 @@ $(document).ready(function() {
         - getPadding($('.chart-content'));
     var test = atest + ($("#chord_id").width()) / 2;
 
+    testSetLineBreaks();
+
     console.log(test);
+});
+
+$(window).resize(function() {
+    testSetLineBreaks();
 });
