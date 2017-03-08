9d07cc86b618ca53f2a8adb9e850a1323b38b377
George  Dosher III <cgartist15@gmail.com>
2017-03-08 11:43:42 -0600

    (QS) Working on code to parse out tags and cleanly handle them

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,122 @@
+$(document).ready(function() {
+
+    processTag("{/c}");
+
+});
+
+// --- Tag Processing ---
+
+function processTag(/*string*/sTag) {
+    //Declare all variables:
+    var sTagName, sTagContents,
+        isEnd = false;
+
+    //Remove braces
+    sTag = removeEnclosingBraces(sTag)
+    //Get tag name and contents
+    sTagName = getTagName(sTag);
+    sTagContents = sTag.substr(sTagName.length).trim();
+
+    //Check if it is an ending tag
+    if(sTagName.charAt(0) == '/') {
+        isEnd = true;
+        sTagName = sTagName.substr(1);
+    }
+
+    //Pass information to correct function
+    switch(sTagName) {
+        case "c":
+            isEnd ? processEndChordTag() : processChordTag(sTagContents);
+            break;
+        case "br":
+            processBreakTag(sTagContents);
+            break;
+        default:
+            console.log("unknown tag: " + sTagName);
+            return;
+    }
+
+    //Test
+    var test = "nothing";
+    console.log(test);
+}
+
+// Chord Tag ---
+
+function ChordManger() {
+
+    var id = 0;
+
+    this.open = false; //bool: are we in the middle of placing a chord?
+
+    this.incrementId = function() {
+        id++;
+        return getId;
+    }
+    this.getId = function() {return id};
+}
+
+function processChordTag(sTagContents) {
+    console.log("processChord");
+
+    //jobs:
+    //- Add 'add-chord' element to line above in non exists
+    //- Add chord to 'add-chord'
+    //- Add 'chord_loc' to this position
+}
+
+function processEndChordTag() {
+    console.log("processEndChord");
+
+    //jobs:
+    //- End 'chord_loc'
+    //- Position corresponding chord
+}
+
+// Break Tag ---
+
+function processBreakTag(sTagContents) {
+    console.log("processBreak");
+}
+
+// --- Helper Funcions ---
+
+function removeEnclosingBraces(/*string*/str) {
+    str = str.trim();
+
+    if(str.charAt(0) == '{') {
+        str = str.substr(1);
+    }
+
+    if(str.charAt(str.length-1) == '}') {
+        str = str.substring(0, str.length-1);
+    }
+
+    return str;
+}
+
+function getTagName(/*string*/str) {
+    var name = "",
+        i = 0;
+
+    //Make sure trimmed
+    str = str.trim();
+
+    //Get chars before space or '='
+    while(str.charAt(i) != ' ' && str.charAt(i) != '=' && i < str.length) {
+        name += str.charAt(i);
+        i++;
+    }
+
+    return name.trim();
+}
+
+// --- REFERENCE CODE ---
+
 //Needs to be cleaned up a lot, but works
-function goodVersion() {
+function chordsOverWordsGoodVersion() {
     var textFileStr = getTextFile("src/testFiles/test.txt");
-    var div = $('#readInDoc');
+    var parentDiv = $('#readInDoc');
     var nHtml = "";
 
     //State
@@ -63,7 +178,7 @@ function goodVersion() {
 
         nHtml+=char;
     }
-    div.append(nHtml);
+    parentDiv.append(nHtml);
 
     chords = $('.chord');
     chords.each(function(i,x) {
@@ -78,8 +193,4 @@ function goodVersion() {
     })
 }
 
-$(document).ready(function() {
 
-    goodVersion();
-
-});
